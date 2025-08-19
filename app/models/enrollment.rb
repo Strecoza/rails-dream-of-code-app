b6b3942b9f1860c10480d3c61da17c6f61769f34
TatianaPri <87344989+Strecoza@users.noreply.github.com>
2025-08-18 21:02:53 -0700

    tp/lesson-5-assignment

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,4 +2,14 @@ class Enrollment < ApplicationRecord
   belongs_to :course
   belongs_to :student
   has_many :mentor_enrollment_assignments
+
+  def is_past_application_deadline
+    deadline = course.trimester.application_deadline
+
+    if deadline && self.created_at > deadline.end_of_day
+      true
+    else
+      false
+    end
+  end
 end
